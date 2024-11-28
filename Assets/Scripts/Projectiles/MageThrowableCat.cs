500623e48abde3dbeb5737340ef173be1dfcb101
KGoroshnikov <goroshnikovk1@gmail.com>
2024-11-28 20:41:48 +0300

    CatFixes

--- Assets/Scripts/Projectiles/MageThrowableCat.cs
+++ Assets/Scripts/Projectiles/MageThrowableCat.cs
@@ -1,38 +1,57 @@
 ﻿using UnityEngine;
 using UnityEngine.Serialization;
 
-namespace Projectiles
+public class MageThrowableCat : ThrowableCat
 {
-    public class MageThrowableCat : ThrowableCat
-    {
-        [SerializeField] private GameObject fireballPrefab;
-        [SerializeField] private float fireballSpeed = 100;
-        [SerializeField] private float fireballSpawnDistance = 3;
-        
-        private bool abilityUsed;
-        
-        
-        protected void InFlightUpdate()
-        {
-            if (abilityUsed) return;
-            if (!Input.GetMouseButton(0)) return;
-            
-            var camera = Camera.main;
-            if (!camera) return;
-            var ray = camera.ScreenPointToRay(Input.mousePosition);
-            if (!Physics.Raycast(ray, out var hit)) return;
-            Debug.Log("Mage Activated!");
-            SendFireball(hit);
-            abilityUsed = false;
-        }
+    [SerializeField] private GameObject fireballPrefab;
+    [SerializeField] private float fireballSpeed = 100;
+    [SerializeField] private float fireballSpawnDistance = 3;
+    
+    private bool abilityUsed;
+    private float maxHeight;
+    private bool waitingToFireball;
+    private Transform king;
 
-        private void SendFireball(RaycastHit hit)
-        {
-            var dir = (hit.point - transform.position).normalized;
-            var projectile = Instantiate(fireballPrefab, transform.position + dir * fireballSpawnDistance, Quaternion.identity, null);
-            projectile.transform.forward = dir;
-            if (projectile.TryGetComponent(out Rigidbody rb))
-                rb.AddForce(dir * fireballSpeed, ForceMode.Impulse);
+    void Start(){
+        king = GameObject.FindWithTag("KingRat").transform;
+    }
+
+    public override void Launch(Vector3 vel)
+    {
+        base.Launch(vel);
+        maxHeight = 0;
+        waitingToFireball = true;
+    }
+
+    void FixedUpdate(){
+        if (!waitingToFireball) return;
+        if (transform.position.y >= maxHeight) maxHeight = transform.position.y;
+        else if (!abilityUsed){ // выпускает шар на максимальной высоте
+            SendFireball();
+            abilityUsed = true;
         }
+    }
+
+    private void SendFireball()
+    {
+        GameObject[] allrats = GameObject.FindGameObjectsWithTag("Rat");
+        Transform closetsRat;
+        if (allrats.Length > 0){
+            closetsRat = allrats[0].transform;
+            float distClosest = Vector3.Distance(transform.position, closetsRat.position);
+            foreach(GameObject rat in allrats)
+                if (Vector3.Distance(transform.position, rat.transform.position) < distClosest){
+                    distClosest = Vector3.Distance(transform.position, rat.transform.position);
+                    closetsRat = rat.transform;
+                }
+        }
+        else closetsRat = king;
+
+        var dir = new Vector3(Random.value, Random.value, 0);
+        if (closetsRat != null) dir = (closetsRat.position - transform.position).normalized;
+        var projectile = Instantiate(fireballPrefab, transform.position + dir * fireballSpawnDistance, Quaternion.identity, null);
+        projectile.transform.forward = dir;
+        if (projectile.TryGetComponent(out Rigidbody rb))
+            rb.AddForce(dir * fireballSpeed, ForceMode.Impulse);
     }
 }
