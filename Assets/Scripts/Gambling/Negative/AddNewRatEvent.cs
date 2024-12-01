386948d51c40b53bd5c9d57ab64827e756612829
Alexander <sasha.izmyloff@mail.ru>
2024-12-01 03:14:10 +0300

    Wheel On Canvas

--- Assets/Scripts/Gambling/Negative/AddNewRatEvent.cs
+++ Assets/Scripts/Gambling/Negative/AddNewRatEvent.cs
@@ -6,6 +6,11 @@ namespace Gambling.Negative
     public class AddNewRatEvent : GamblingEvent
     {
         public override string Name => "Добавление крысы";
-        public override void Execute(GamblingManager manager) => Instantiate(manager.ratTowerPrefab, manager.ratTowerParent);
+        public override void Execute(GamblingManager manager)
+        {
+            var parent = Random.Range(0, manager.ratTowerParentVariants.Count);
+            Instantiate(manager.ratTowerPrefab, manager.ratTowerParentVariants[parent]);
+            manager.ratTowerParentVariants.RemoveAt(parent);
+        }
     }
 }
